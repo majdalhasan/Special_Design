237bf7534e648ae170864f1530021e7b99ed73db
Majd ALhasan <94524369+majdalhasan@users.noreply.github.com>
2023-03-13 10:16:27 +0300

    Add files via upload

--- js/master.js
+++ js/master.js
@@ -3,7 +3,27 @@ let mainColors = localStorage.getItem("color_option");
 
 if (mainColors !== null) {
 
-  document.documentElement.style.setProperty('--main-color',localStorage.getItem("color_option"));
+  document.documentElement.style.setProperty('--main-color',mainColors);
+
+  //Remove Active Class From All Colors List Item
+  document.querySelectorAll(".colors-list li").forEach(Element => {
+
+    Element.classList.remove("active");
+
+
+    //Add Active Class On Element With Data-Color === Local Storage Item
+    if (Element.dataset.color === mainColors) {
+
+      //Add Active Class
+      Element.classList.add("active");
+
+    }
+
+
+  });
+
+
+
 
 }
 
