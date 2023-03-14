896245e66d8378e418f4b99f68d2df4280d07e68
Majd ALhasan <94524369+majdalhasan@users.noreply.github.com>
2023-03-14 10:07:09 +0300

    Add files via upload

--- js/master.js
+++ js/master.js
@@ -68,6 +68,30 @@ colorsLi.forEach(li => {
 });
 
 
+//Switch Random Background Option
+const randomBackEl = document.querySelectorAll(".random-backgrounds span");
+
+//Loop On All Spans
+randomBackEl.forEach(span => {
+
+  //Click On Every Span
+  span.addEventListener("click", (e) => {
+
+    //Remove Active Class From All Spans
+    e.target.parentElement.querySelectorAll(".active").forEach(Element => {
+
+      Element.classList.remove("active");
+
+    });
+
+    //Add Active Class On Self
+    e.target.classList.add("active");
+
+  });
+
+});
+
+
 //Select Landing Page Element
 let landingPage=document.querySelector(".landing-page");
 
